ca6e435c0f2effd6caa06ffe820b0de341a89568
grd <892991132@qq.com>
2018-07-07 01:37:27 +0800

    第一次提交

---
+++ git-test/src/cn/itcast/git/demo/GitTest.java
@@ -0,0 +1,8 @@
+package cn.itcast.git.demo;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test1");
+        System.out.println("git test2");
+    }
+}
